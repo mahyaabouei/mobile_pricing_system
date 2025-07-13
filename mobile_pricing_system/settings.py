5f0ba22f8c7bcb54f4e217f2e986591a7807d086
mahyaabouei <mahyaaboueii@gmail.com>
2025-07-13 22:30:20 +0330

    swagger

--- mobile_pricing_system/settings.py
+++ mobile_pricing_system/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'store',
     'rest_framework_simplejwt.token_blacklist',
     'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -143,3 +144,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
