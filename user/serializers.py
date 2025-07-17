f006bac1d8abc673659eef9dfba1b9850c9c96ed
moeendehqan <moeen.dehqan@gmail.com>
2025-07-17 21:26:57 +0330

    PermissionSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -3,7 +3,16 @@ from django.contrib.auth import get_user_model
 from .models import User , Otp
 
 
+from django.contrib.auth.models import Permission
+
+class PermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Permission
+        fields = ['id', 'name', 'codename', 'content_type']
+
 class UserSerializer(serializers.ModelSerializer):
+    user_permissions = PermissionSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
         fields = '__all__'
@@ -37,3 +46,4 @@ class UserInputSerializer(serializers.Serializer):
     card_number = serializers.CharField(required=True, allow_blank=True)
     account_number = serializers.CharField(required=True, allow_blank=True)
     account_bank = serializers.CharField(required=True, allow_blank=True)
+
